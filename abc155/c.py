c9387fa1dae77576964b825090058ba457d0576f
necocoa <joyds217@gmail.com>
2020-02-22 20:50:22 +0900

    ABC155 C AC ref for input, output

--- abc155/c.py
+++ abc155/c.py
@@ -2,15 +2,13 @@ from collections import Counter
 
 
 def main():
-    N = int(input())
-    A = [input() for _ in range(N)]
+    _, *S = open(0).read().split()
 
-    c = Counter(A)
+    c = Counter(S)
     max_num = c.most_common()[0][1]
-    ANS = [i[0] for i in c.items() if i[1] == max_num]
-    ANS.sort()
-    for ans in ANS:
-        print(ans)
+    ans = [i[0] for i in c.items() if i[1] == max_num]
+    ans.sort()
+    print("\n".join(ans))
 
 
 if __name__ == '__main__':
