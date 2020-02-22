7598a51cfeaabb9047a43f8d6aa6001f54bff973
necocoa <joyds217@gmail.com>
2020-02-22 20:45:16 +0900

    ABC155 D TLE

--- abc155/d.py
+++ abc155/d.py
@@ -1,12 +1,26 @@
 n, k = map(int, input().split())
-ar = list(map(int, input().split()))
+A = sorted(list(map(int, input().split())))
+
+print('A: %s' % A)
 
 cnt = n - 1
-ps = []
-for a in ar:
-    for num in range(cnt):
-        ps.append(a * ar[-(num + 1)])
-    cnt -= 1
+ans = []
 
-ans = sorted(ps)
-print(ans[k-1])
+i = 0
+count = 0
+for a in A:
+    for num in range(cnt):
+        ans.append(a * A[i + num + 1])
+        count += 1
+        if k == count:
+            break
+    else:
+        i += 1
+        cnt -= 1
+        continue
+    break
+
+print('count: %s' % count)
+print('ans: %s' % ans)
+sorted_ans = sorted(ans)
+print(sorted_ans[k-1])
