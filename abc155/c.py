905593583879ce63926409f99f8bb86aed7ab702
necocoa <joyds217@gmail.com>
2020-02-22 20:54:14 +0900

    ABC155 C AC ref counter.values

--- abc155/c.py
+++ abc155/c.py
@@ -5,8 +5,8 @@ def main():
     _, *S = open(0).read().split()
 
     c = Counter(S)
-    max_num = c.most_common()[0][1]
-    ans = [i[0] for i in c.items() if i[1] == max_num]
+    max_cnt = max(c.values())
+    ans = [k for k, v in c.items() if v == max_cnt]
     ans.sort()
     print("\n".join(ans))
 
