be29beb82b0e2b95ce931082d04b4f213b25b870
necocoa <43537209+necocoa@users.noreply.github.com>
2020-04-12 23:35:51 +0900

    ABC162 B AC

---
+++ abc162/b.py
@@ -0,0 +1,11 @@
+def main():
+    n = int(input())
+    ans = 0
+    for i in range(n):
+        if (i + 1) % 3 != 0 and (i + 1) % 5 != 0:
+            ans += i + 1
+    print(ans)
+
+
+if __name__ == '__main__':
+    main()
