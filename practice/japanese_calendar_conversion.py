103d04b3316452a56991e310ec236882ca303c3e
necocoa <43537209+necocoa@users.noreply.github.com>
2020-04-26 20:27:50 +0900

    japanese_calendar_conversion v2

--- practice/japanese_calendar_conversion.py
+++ practice/japanese_calendar_conversion.py
@@ -1,4 +1,6 @@
-japanese_calendar_list = {
+from typing import TypeVar, Tuple, Dict
+
+japanese_calendar_list: Dict[int, str] = {
     1868: '明治',
     1912: '大正',
     1926: '昭和',
@@ -6,8 +8,10 @@ japanese_calendar_list = {
     2019: '令和'
 }
 
+Wareki = Tuple[str, int]
 
-def japanese_calendar_conversion(year):
+
+def bs(year: int) -> Tuple[str, int]:
     low = 0
     high = len(japanese_calendar_list) - 1
     jp_calendar_year_list = list(japanese_calendar_list.keys())
@@ -15,18 +19,25 @@ def japanese_calendar_conversion(year):
     while low <= high:
         mid = (low + high) // 2
         if jp_calendar_year_list[mid] <= year < jp_calendar_year_list[mid + 1]:
-            japanese_calendar = japanese_calendar_list[jp_calendar_year_list[mid]]
-            if jp_calendar_year_list[mid] == year:
-                return print("%s元年" % japanese_calendar)
-            else:
-                japanese_calendar_year = year - jp_calendar_year_list[mid] + 1
-                return print("%s%s年" % (japanese_calendar, japanese_calendar_year))
-
+            wareki = japanese_calendar_list[jp_calendar_year_list[mid]]
+            wareki_year = year - jp_calendar_year_list[mid] + 1
+            return wareki, wareki_year
         elif jp_calendar_year_list[mid] < year:
             low = mid + 1
         else:
             high = mid - 1
+    return 'notthing', 0
 
 
-japanese_calendar_conversion(2000)
+def japanese_calendar_conversion(year: int) -> str:
+    wareki, wareki_year = bs(year)
+    if wareki == 'notthing':
+        return ('見つかりませんでした')
+    elif wareki_year == 1:
+        return ("%s元年" % wareki)
+    else:
+        return ("%s%s年" % (wareki, wareki_year))
+
+
+print(japanese_calendar_conversion(2000))
 # 2000 => 平成12年
